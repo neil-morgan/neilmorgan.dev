a7afa7760ed0db01d4ab6121d0457f0d1fab4daf
neil-morgan <neilmorgan.dev@gmail.com>
2023-08-16 15:26:38 +0100

    feat: slashes

--- lib/apollo/client.ts
+++ lib/apollo/client.ts
@@ -16,7 +16,7 @@ const {
 const dbLink = createHttpLink({
   uri: `${
     process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
+      ? `https://${process.env.VERCEL_URL}/`
       : "http://localhost:3000/"
   }api/server`,
 });
