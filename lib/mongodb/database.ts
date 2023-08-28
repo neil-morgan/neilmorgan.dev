3bff64a25d9f4bb91ddb2d0595d8619bffc4b82a
neil-morgan <neilmorgan.dev@gmail.com>
2023-08-28 21:59:16 +0100

    fix: mongodb connection

--- lib/mongodb/database.ts
+++ lib/mongodb/database.ts
@@ -1,45 +1,12 @@
-import { connect } from "mongoose";
+import mongoose from "mongoose";
 
-const { MONGODB_URI } = process.env;
-
-if (!MONGODB_URI || MONGODB_URI.length === 0) {
-  throw new Error("Please add your MongoDB URI to .env.local");
-}
-
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-export const connectDb = async () => {
-  if (cached.conn) {
-    console.log("🚀 Using cached connection");
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-
-    cached.promise = await connect(MONGODB_URI, opts)
-      .then(mongoose => {
-        console.log("✅ New connection established");
-        return mongoose;
-      })
-      .catch(error => {
-        console.error("❌ Connection to database failed");
-        throw error;
-      });
-  }
-
-  try {
-    cached.conn = await cached.promise;
-  } catch (e) {
-    cached.promise = null;
-    throw e;
-  }
-
-  return cached.conn;
-};
+export const connectDb = () =>
+  mongoose
+    .connect(process.env.MONGODB_URI as string)
+    .then(() => {
+      console.log("✅ New connection established");
+    })
+    .catch(err => {
+      console.log("❌ Connection to database failed");
+      throw err;
+    });
