94ae38eea0833e0e290c9643d6957aade54e4a3c
neil-morgan <neilmorgan.dev@gmail.com>
2023-10-22 13:46:23 +0100

    restore home page

--- components/templates/HomePageTemplate/HomePageTemplate.tsx
+++ components/templates/HomePageTemplate/HomePageTemplate.tsx
@@ -1,50 +1,9 @@
 "use client";
 
-import {
-  Button,
-  Container,
-  Heading,
-  Text,
-  HorizontalSeparator,
-} from "@/components/atoms";
-import { styled } from "@/lib/stitches";
-
-const Bg = styled("div", {
-  display: "flex",
-  width: "100%",
-  flexWrap: "wrap",
-  gap: "$4",
-  margin: "$8 0",
-});
-
-const Box = styled("div", {
-  height: "100px",
-  width: "100px",
-  background: "red",
-});
+import { Container, Heading } from "@/components/atoms";
 
 export const HomePageTemplate = () => (
   <Container>
-    {/* <Bg>
-      {Array.from({ length: 12 }, (_, i) => (
-        <Box key={i} css={{ background: `$base${i + 1}` }} />
-      ))}
-    </Bg> */}
-
-    <Bg>
-      {Array.from({ length: 14 }, (_, i) => (
-        <Box key={i} css={{ background: `$base${i + 1}` }} />
-      ))}
-    </Bg>
-    <HorizontalSeparator />
-    {Array.from({ length: 6 }, (_, i) => (
-      <Heading key={i} size={`h${i + 1}`}>
-        Heading
-      </Heading>
-    ))}
-    <Text>This is some text</Text>
-
-    <Button>Button</Button>
-    <Button priority="primary">Button</Button>
+    <Heading>Home page</Heading>
   </Container>
 );
