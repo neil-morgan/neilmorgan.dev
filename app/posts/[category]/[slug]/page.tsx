28b18a6db88e901fd835dab2dfe8f6a3abbcd0b2
neil-morgan <neilmorgan.dev@gmail.com>
2023-12-04 10:20:46 +0000

    force-dynamic

--- app/posts/[category]/[slug]/page.tsx
+++ app/posts/[category]/[slug]/page.tsx
@@ -14,6 +14,8 @@ import {
 } from "@/graphql";
 import { APOLLO_CLIENTS } from "@/lib/site";
 
+export const dynamic = "force-dynamic";
+
 export async function generateMetadata({ params }: SlugMetaProps) {
   const { data } = await getClient().query({
     query: CategoryDocument,
