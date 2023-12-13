57550b7cea8ac341970a565fd9cbf35198862780
neil-morgan <neilmorgan.dev@gmail.com>
2023-12-13 12:15:17 +0000

    home page test

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,38 @@
+import { redirect } from "next/navigation";
+import { draftMode } from "next/headers";
 import { HomePageTemplate } from "@/components/templates";
+import type { SlugProps, SlugMetaProps } from "@/types";
+import { PostTemplate } from "@/components/templates";
+import { getFeatureFlags } from "@/helpers";
+import { getClient } from "@/lib/apollo";
+import {
+  CategoryDocument,
+  TestDocument,
+  Post,
+  CreatePostDataDocument,
+  PostSlugsDocument,
+  PostDataDocument,
+} from "@/graphql";
+import { APOLLO_CLIENTS } from "@/lib/site";
 
-const HomePage = async () => <HomePageTemplate />;
+const HomePage = async () => {
+  const { data } = await getClient().query({
+    context: {
+      fetchOptions: {
+        next: { revalidate: 5 },
+      },
+    },
+    query: TestDocument,
+  });
+
+  console.log(data);
+
+  return (
+    <>
+      {data.post?.title}
+      <HomePageTemplate />
+    </>
+  );
+};
 
 export default HomePage;
