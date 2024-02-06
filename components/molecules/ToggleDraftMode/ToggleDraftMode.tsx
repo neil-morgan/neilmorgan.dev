fd2d0195241e564afee24d458094d961ea287d68
neil-morgan <neilmorgan.dev@gmail.com>
2024-02-06 15:22:53 +0000

    preview secret

--- components/molecules/ToggleDraftMode/ToggleDraftMode.tsx
+++ components/molecules/ToggleDraftMode/ToggleDraftMode.tsx
@@ -10,7 +10,7 @@ export const ToggleDraftMode = ({ isEnabled }: { isEnabled: boolean }) => {
       href={
         isEnabled
           ? `/api/disable-draft?redirect=${pathname}`
-          : `/api/draft?redirect=${pathname}&secret=${process.env.NEXT_PUBLIC_CONTENTFUL_PREVIEW_SECRET}`
+          : `/api/draft?redirect=${pathname}&secret=contentpreview`
       }
       isEnabled={isEnabled}>
       {isEnabled ? "Exit Draft Mode" : "Draft Mode"}
