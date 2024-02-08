fe7950f7e34db8c0e77fd384689638f95b34db3d
neil-morgan <neilmorgan.dev@gmail.com>
2024-02-08 12:14:53 +0000

    including code string comp

---
+++ components/atoms/CodeString/styles.ts
@@ -0,0 +1,11 @@
+"use client";
+
+import { styled } from "@/lib/stitches";
+
+export const CodeStringElement = styled("code", {
+  position: "relative",
+  background: "$primary2",
+  borderRadius: "$sm",
+  padding: "0.175rem 0.4rem 0.25rem",
+  fontFamily: "var(--font-roboto-mono)",
+});
