f11419983218c166d8f1352049b92cda7653d2f9
neil-morgan <neilmorgan.dev@gmail.com>
2024-02-14 11:07:52 +0000

    correctly specify not found

--- app/posts/error.tsx
+++ app/posts/error.tsx
@@ -3,16 +3,9 @@
 import { InfoMessage } from "@/components/molecules";
 import { INFO_MESSAGES } from "@/lib/site";
 
-const { pageNotFound, errorOccurred } = INFO_MESSAGES;
-
-export default function Error({
-  error,
-}: {
+export default function Error({}: {
   error: Error & { digest?: string };
   reset: () => void;
 }) {
-  const message =
-    error.message === "NEXT_NOT_FOUND" ? pageNotFound : errorOccurred;
-
-  return <InfoMessage {...message} />;
+  return <InfoMessage {...INFO_MESSAGES.errorOccurred} />;
 }
